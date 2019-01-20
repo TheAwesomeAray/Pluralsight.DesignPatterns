187c6165e48ca8e585335c454e44b85ff20e1c19
TheAwesomeARay <andrewray4@gmail.com>
2019-01-20 17:00:32 -0600

    Iterator

---
+++ Pluralsight.DesignPatterns/Iterator/Tree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pluralsight.DesignPatterns.Iterator
+{
+    public class Tree<T>
+    {
+        public Tree<T> LeftChild { get; set; }
+        public Tree<T> RightChild { get; set; }
+
+        
+    }
+}
---
+++ Pluralsight.DesignPatterns/Iterator/TreeEnumerator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pluralsight.DesignPatterns.Iterator
+{
+    public class TreeEnumerator<T>
+    {
+        private Tree<T> _tree;
+        public Tree<T> Current { get; private set; }
+        private Tree<T> Previous { get; set; }
+        private Tree<T> Next { get; set; }
+        private Stack<Tree<T>> _breadCrumb;
+
+        public TreeEnumerator(Tree<T> tree)
+        {
+            _tree = tree;
+        }
+
+        public bool MoveNext()
+        {
+            if (Current == null)
+            {
+                Reset();
+                Current = _tree;
+                return true;
+            }
+            if (Current.LeftChild != null)
+            {
+                return TraverseLeft();
+            }
+            if (Current.RightChild != null)
+            {
+                return TraverseRight();
+            }
+            return TraverseUpAndRight();
+        }
+
+        private bool TraverseUpAndRight()
+        {
+            if (_breadCrumb.Count > 0)
+            {
+                Previous = Current;
+                while (true)
+                {
+                    Current = _breadCrumb.Pop();
+                    if (Previous != Current.RightChild)
+                        break;
+                }
+                if (Current.RightChild != null)
+                {
+                    _breadCrumb.Push(Current);
+                    Current = Current.RightChild;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool TraverseRight()
+        {
+            _breadCrumb.Push(Current);
+            Current = Current.RightChild;
+            return true;
+        }
+
+        private bool TraverseLeft()
+        {
+            _breadCrumb.Push(Current);
+            Current = Current.LeftChild;
+            return true;
+        }
+
+        private void Reset()
+        {
+            Current = null;
+        }
+    }
+}
