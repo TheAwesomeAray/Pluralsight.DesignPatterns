9838466e8433803e9a937627e071323006d06cc9
TheAwesomeARay <andrewray4@gmail.com>
2019-01-21 18:18:02 -0600

    MVVM

---
+++ Pluralsight.DesignPatterns/MVVM/Model/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Pluralsight.DesignPatterns.MVVM
+{
+    public class Person : INotifyPropertyChanged, IDataErrorInfo
+    {
+        private string _FirstName;
+
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set
+            {
+                _FirstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        private string _LastName;
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set
+            {
+                _LastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        private DateTime _UpdatedDate;
+
+        public DateTime UpdatedDate
+        {
+            get { return _UpdatedDate; }
+            set
+            {
+                _UpdatedDate = value;
+                OnPropertyChanged("UpdatedDate");
+            }
+        }
+
+        public string Error => null;
+
+        public string this[string columnName] => string.IsNullOrEmpty(_FirstName) ? "First Name is required." : string.Empty;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ Pluralsight.DesignPatterns/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+
+namespace Pluralsight.DesignPatterns.MVVM
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Person _ModelPerson;
+
+        public Person ModelPerson
+        {
+            get { return _ModelPerson; }
+            set
+            {
+                _ModelPerson = value;
+                OnPropertyChanged("ModelPerson");
+            }
+        }
+        public MainViewModel()
+        {
+            ModelPerson = LoadPerson();
+        }
+
+        public MainViewModel(Person person)
+        {
+            _ModelPerson = person;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        private Person LoadPerson()
+        {
+            return new Person()
+            {
+                FirstName = "Andrew",
+                LastName = "Ray"
+            };
+        }
+    }
+}
