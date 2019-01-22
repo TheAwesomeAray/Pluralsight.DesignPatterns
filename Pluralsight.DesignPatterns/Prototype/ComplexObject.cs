fbd2b17e74c1a9c2878014d89e037be8aee867f0
TheAwesomeARay <andrewray4@gmail.com>
2019-01-22 14:44:51 -0600

    Prototype

---
+++ Pluralsight.DesignPatterns/Prototype/ComplexObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pluralsight.DesignPatterns.Prototype
+{
+    public class ComplexObject : ICloneable
+    {
+        internal ComplexObject(string param1, string param2, string param3)
+        {
+
+        }
+
+        internal ComplexObject(string param1, string param2, string param3, string param4)
+        {
+
+        }
+
+        public object Clone()
+        {
+            //Outside of assembly, constructors cannot be used. Allow users to be able to create copies
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ Pluralsight.DesignPatterns/Prototype/WebPageScraper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+
+namespace Pluralsight.DesignPatterns.Prototype
+{
+    class WebPageScraper : ICloneable
+    {
+        public string title { get; set; }
+        public int headerTagCount { get; set; }
+        private string firstHeaderTagContents { get; set; }
+
+        public WebPageScraper(string url)
+        {
+            var client = new WebClient();
+            Scrape(client.DownloadString(url));
+        }
+
+        private void Scrape(string v)
+        {
+            title = "Fake Title";
+            headerTagCount = 3;
+            firstHeaderTagContents = "Fake Header text";
+        }
+
+        public void PrintPageData()
+        {
+            Console.WriteLine($"Title: {title}, Header Count: {headerTagCount}, First Header: {firstHeaderTagContents}");
+        }
+
+        public object Clone()
+        {
+            //object is expensive to create. Cheap to copy
+            return MemberwiseClone();
+        }
+    }
+}
