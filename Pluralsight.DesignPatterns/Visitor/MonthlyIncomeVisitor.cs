f44a24ff4fa35c2d161c062c859966c00d8008cb
TheAwesomeARay <andrewray4@gmail.com>
2019-01-23 23:06:02 -0600

    Another Visitor Example

---
+++ Pluralsight.DesignPatterns/Visitor/MonthlyIncomeVisitor.cs
@@ -0,0 +1,22 @@
+﻿namespace Pluralsight.DesignPatterns.Visitor
+{
+    public class MonthlyIncomeVisitor : IVisitor
+    {
+        public decimal MonthlyIncome { get; set; }
+
+        public void Visit(BankAccount bankAccount)
+        {
+            MonthlyIncome += (bankAccount.Amount * bankAccount.MonthlyInterest);
+        }
+
+        public void Visit(RealEstate realEstate)
+        {
+            MonthlyIncome += realEstate.MonthlyRent;
+        }
+
+        public void Visit(Loan loan)
+        {
+            MonthlyIncome -= loan.MinimumMonthlyPayment;
+        }
+    }
+}
