92d64bf4d299100170f333584ff90895d459aceb
TheAwesomeARay <andrewray4@gmail.com>
2019-01-23 19:43:49 -0600

    Template

---
+++ Pluralsight.DesignPatterns/Template/OrderShipment.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace Pluralsight.DesignPatterns.Template
+{
+    public abstract class OrderShipment
+    {
+        public string ShippingAddress { get; set; }
+        public string Label { get; set; }
+
+        public void Ship(TextWriter writer)
+        {
+            VerifyShippingData();
+            SetShippingLabelFromCarrier();
+            PrintLabel(writer);
+        }
+
+        public abstract void VerifyShippingData();
+        public abstract void SetShippingLabelFromCarrier();
+        public abstract void PrintLabel(TextWriter writer);
+    }
+}
---
+++ Pluralsight.DesignPatterns/Template/UpsOrderShipment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Pluralsight.DesignPatterns.Template
+{
+    public class UpsOrderShipment : OrderShipment
+    {
+        public override void PrintLabel(TextWriter writer)
+        {
+            writer.Write(Label);
+        }
+
+        public override void SetShippingLabelFromCarrier()
+        {
+            Label = $"UPS: {ShippingAddress}";
+        }
+
+        public override void VerifyShippingData()
+        {
+            if (string.IsNullOrEmpty(ShippingAddress))
+            {
+                throw new ApplicationException("Invalid Address");
+            }
+        }
+    }
+}
